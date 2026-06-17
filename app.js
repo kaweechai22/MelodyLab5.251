42747de803271ed076eb529ec730c59b118a3c43
kaweechai22 <cacarosjampa@gmail.com>
2026-06-17 22:30:35 +0700

    Add files via upload

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const $=id=>document.getElementById(id);
-/* v5.207 add rebuilt beats visualizer */
+/* v5.209 beats text trim */
 const rebuiltTopicModes = new Set(["soundReflection","soundRefraction","soundDiffraction","soundInterference","superposition","beatsViz","standingAir","resonanceViz","resonanceAirHarmonics","dopplerViz","shockWave","soundIntensity","soundIntensityLevel","soundLevelHearing","harmonicsViz","noisePollution","applicationsSound"]);
 const soundTopicModes = rebuiltTopicModes;
 
